162b7aa6d9cc4754906fa5e382c1f010123cd16c
ftmendonca09-ui <ftmendonca09@gmail.com>
2026-04-07 13:36:48 -0300

    Update OrdemServico.kt

--- OrdemServico.kt
+++ OrdemServico.kt
@@ -1,1 +1,18 @@
+data class OrdemServico(
 
+    val cliente: String,
+    val veiculo: String,
+    val placa: String,
+    val servico: String,
+    val descricao: String,
+
+    var status: String,
+
+    var lixamento: Boolean,
+    var pintura: Boolean,
+    var polimento: Boolean,
+
+    val tempoEstimado: String,
+    var tempoReal: String
+
+)
